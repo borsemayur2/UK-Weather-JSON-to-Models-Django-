996e70c289dda915219bbbfefcba965bccb743fe
borsemayur2 <47341981+borsemayur2@users.noreply.github.com>
2019-03-01 11:54:22 +0530

    Update fetch.py

--- kisanhub/ukweather/management/commands/fetch.py
+++ kisanhub/ukweather/management/commands/fetch.py
@@ -1,22 +1,27 @@
 
 import requests
-metrics = ['Tmax', 'Tmin', 'Rainfall']
-locations = ['UK', 'England', 'Scotland', 'Wales']
-data=[]
-for metric in metrics:
-	for location in locations:
-		print(metric)
-		print(location)
-		a=requests.get('https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/%s-%s.json'%(metric,location))
-		d=a.json()
-		data.append(d)		
-		'''value=[]
-		year=[]
-		month=[]
-		for i in range(3):
-			value.append(d[i]['value'])
-			year.append(d[i]['year'])
-			month.append(d[i]['month'])
-			print(year[i],month[i], value[i] )
-			print(data)'''
+from django.core.management import BaseCommand
+class Command(BaseCommand):
+	
+	def handle(*args, **options):
+		metrics = ['Tmax', 'Tmin', 'Rainfall']
+		locations = ['UK', 'England', 'Scotland', 'Wales']
+		data=[]
+		for metric in metrics:
+			for location in locations:
+				print(metric)
+				print(location)
+				a=requests.get('https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/%s-%s.json'%(metric,location))
+				d=a.json()
+				data.append(d)		
+				value=[]
+				year=[]
+				month=[]
+				for i in range(3):
+					value.append(d[i]['value'])
+					year.append(d[i]['year'])
+					month.append(d[i]['month'])
+					print(year[i],month[i], value[i] )
+					print(data)
+				
 		
